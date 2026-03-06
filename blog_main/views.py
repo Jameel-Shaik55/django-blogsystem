0379aacc411139a92a400a1c0ce0163724a7e59c
Jameel-Shaik55 <jameelsk655@gmail.com>
2026-03-06 22:20:01 +0530

    blogs and search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,16 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Category, Blog
+from aside.models import About
 
 def home(request):
-    categories = Category.objects.all()
+    # THis return all the categories from the category table
+    # categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status = "Published").order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status = "Published")
+
+    try:
+        about = About.objects.get()
+    except:
+        about = None
     # print(categories.query)
-    print(featured_posts)
+    # print(featured_posts)
     context = {
-        'categories': categories,
+        # 'categories': categories,
         'featured_posts': featured_posts,
-        'posts': posts
+        'posts': posts,
+        'about':about,
     }
     return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 def get_posts_by_category(request, category_id):
@@ -22,3 +23,23 @@ def get_posts_by_category(request, category_id):
         'category': category
     }
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status = "Published")
+    # print(single_blog)
+
+    context = {
+        'single_blog': single_blog
+    }
+    return render(request, 'single_blog_page.html', context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status="Published")
+
+    context = {
+        'blogs':blogs,
+        'keyword':keyword,
+    }
+
+    return render(request, 'search.html', context)
